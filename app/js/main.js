b1b32e0ab9de7c1aa71e6eecda50a13e684da1ce
cpolikk <cpolikk2@gmail.com>
2021-10-17 12:51:31 +0300

    added blog and partners section

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,19 @@ $(function () {
         autoplaySpeed: 3000
     });
 
-    var mixer = mixitup('.products__items');
-    var mixer = mixitup('.products__grid');
+    // var mixer = mixitup('.products__items');
+    // var mixer = mixitup('.products__grid');
 
+    var containerEl1 = $('[data-ref="container-1"]');
+    var containerEl2 = $('[data-ref="container-2"]');
+
+    var config = {
+        controls: {
+            scope: 'local'
+        }
+    };
+
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
 
 });
